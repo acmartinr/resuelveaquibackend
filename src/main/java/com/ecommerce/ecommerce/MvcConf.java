b0f98e09d458440ca1136ed4fa3fc65289b7b240
Gabriel <gmonteagudomartinez96@gmail.com>
2022-05-26 15:48:47 -0400

    Configuracion del proyecto

---
+++ src/main/java/com/ecommerce/ecommerce/MvcConf.java
@@ -0,0 +1,29 @@
+package com.ecommerce.ecommerce;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class MvcConf implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+       WebMvcConfigurer.super.addResourceHandlers(registry);
+       exposeDirectory("product-images", registry);
+      // registry.addResourceHandler("/imagenEcc/**").addResourceLocations("file:/C:/imagenEcc/");
+
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+        //=> for linux
+        //    registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:///"+ uploadPath + "/");
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+    }
+}
