babf91a4ad75b1bf02cbae0226f5def996145fb0
Gabriel <gmonteagudomartinez96@gmail.com>
2022-05-31 17:16:35 -0400

    Nothing important

--- src/main/java/com/ecommerce/ecommerce/Utils/FileUploadUtil.java
+++ src/main/java/com/ecommerce/ecommerce/Utils/FileUploadUtil.java
@@ -3,9 +3,12 @@ package com.ecommerce.ecommerce.Utils;
 import java.io.*;
 import java.nio.file.*;
 
+
 import org.springframework.web.multipart.MultipartFile;
 
+
 public class FileUploadUtil {
+    private Path path;
 
     public static void saveFile(String uploadDir, String fileName,
                                 MultipartFile multipartFile) throws IOException {
@@ -22,4 +25,5 @@ public class FileUploadUtil {
             throw new IOException("Could not save image file: " + fileName, ioe);
         }
     }
+
 }
