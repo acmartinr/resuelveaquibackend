c1ef3f5fe4b06d5442fe92d2892a6021e48fe1ab
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-06 16:42:47 -0400

    Update User.java
    Has been added the fields of firstname, lastname, password and a constructor

--- src/main/java/com/ecommerce/ecommerce/Models/User.java
+++ src/main/java/com/ecommerce/ecommerce/Models/User.java
@@ -9,6 +9,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO )
     private Long id;
+
+    @Column(name="firstnames")
+    private String firstname;
+
+
+
+    @Column(name="lastnames")
+    private String lastname;
     @Column(name="usernames")
     private String username;
 
@@ -23,6 +31,17 @@ public class User {
 
     private String token;
 
+    public User(){
+        super();
+    }
+    public User(String firstname, String lastname, String username, String email, String password){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+
 
     public Long getId() {
         return id;
@@ -30,6 +49,22 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public String getUsername() {
