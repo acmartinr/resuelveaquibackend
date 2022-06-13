dccbf0cccb872b28aedd9e950c7b49d32d51bfce
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-13 17:36:17 -0400

    Update ProductoRepository.java
    abstract method findFirstByCode was created

--- src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
@@ -4,4 +4,5 @@ import com.ecommerce.ecommerce.Models.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
+    Producto findFirstByCode(String codigo);
 }
