a79a746a07e607fe81cc64e0d2dd3d4205d53411
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-13 17:35:38 -0400

    Update JwtResponse.java
    Fields firstname,lastname and address were added to class

--- src/main/java/com/ecommerce/ecommerce/payload/response/JwtResponse.java
+++ src/main/java/com/ecommerce/ecommerce/payload/response/JwtResponse.java
@@ -6,14 +6,20 @@ public class JwtResponse {
     private String token;
     private String type = "Bearer";
     private Long id;
+    private String firstname;
+    private String lastname;
     private String username;
     private String email;
+    private String address;
 
-    public JwtResponse(String accessToken, Long id, String username, String email) {
+    public JwtResponse(String accessToken, Long id, String firstname,String lastname, String username, String email,String address) {
         this.token = accessToken;
         this.id = id;
+        this.firstname=firstname;
+        this.lastname=lastname;
         this.username = username;
         this.email = email;
+        this.address=address;
     }
 
     public String getAccessToken() {
@@ -40,6 +46,22 @@ public class JwtResponse {
         this.id = id;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -56,4 +78,11 @@ public class JwtResponse {
         this.username = username;
     }
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
