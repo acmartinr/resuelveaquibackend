8786cce8c36f62b5483787895aaffb0c5e23d5ef
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-14 15:18:31 -0400

    Update SalesController.java
    Controller to handle sales. Method createSale was added

--- src/main/java/com/ecommerce/ecommerce/Controller/SalesController.java
+++ src/main/java/com/ecommerce/ecommerce/Controller/SalesController.java
@@ -1,24 +1,41 @@
 package com.ecommerce.ecommerce.Controller;
 
+import com.ecommerce.ecommerce.Models.Producto;
 import com.ecommerce.ecommerce.Models.Sale;
 import com.ecommerce.ecommerce.Repository.SalesRepository;
+import com.ecommerce.ecommerce.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
-@RequestMapping(path = "/sales")
+@RequestMapping(path = "/api/sales")
 public class SalesController {
     @Autowired
     SalesRepository salesRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping(value = "/")
     public ResponseEntity<List<Sale>> showSales() {
         List<Sale> sales = salesRepository.findAll();
         return ResponseEntity.ok(sales);
+    }
+
+    @PostMapping(value="/create_sale")
+    public ResponseEntity<String> createSale(@RequestPart Producto[] productos, @RequestPart long id) throws IOException {
+        Sale sale=new Sale();
+        Set<Producto> products=new HashSet<>(Arrays.asList(productos));
+        sale.setProducts(products);
+        sale.setUserShopping(userRepository.findById(id).get());
+        return ResponseEntity.ok("Venta realizada correctamente");
     }
 }
