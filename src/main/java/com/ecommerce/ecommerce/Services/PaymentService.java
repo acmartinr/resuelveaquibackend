216b26153aac666b2944069f2f0cf7876b2d2f55
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-29 16:40:35 -0400

    Create PaymentService.java
    Service for payment

---
+++ src/main/java/com/ecommerce/ecommerce/Services/PaymentService.java
@@ -0,0 +1,65 @@
+package com.ecommerce.ecommerce.Services;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import com.ecommerce.ecommerce.Models.PaymentRequest;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+
+@Service
+public class PaymentService {
+
+    @Value("${STRIPE_SECRET_KEY}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        Stripe.apiKey = secretKey;
+    }
+    public String charge(PaymentRequest chargeRequest) throws StripeException {
+        Map<String, Object> chargeParams = new HashMap<>();
+        chargeParams.put("amount", chargeRequest.getAmount());
+        chargeParams.put("currency", PaymentRequest.Currency.INR);
+        chargeParams.put("source", chargeRequest.getToken().getId());
+
+        Charge charge = Charge.create(chargeParams);
+        return charge.getId();
+    }
+
+    public PaymentIntent paymentIntent(PaymentRequest paymentRequest) throws StripeException {
+        Stripe.apiKey = secretKey;
+        List<String> paymentMethodTypes = new ArrayList();
+        paymentMethodTypes.add("card");
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentRequest.getAmount());
+        params.put("currency", paymentRequest.getCurrency());
+        params.put("description", paymentRequest.getDescription());
+        params.put("payment_method_types", paymentMethodTypes);
+        return PaymentIntent.create(params);
+    }
+
+    public PaymentIntent confirm(String id) throws StripeException {
+        Stripe.apiKey = secretKey;
+        PaymentIntent paymentIntent = PaymentIntent.retrieve(id);
+        Map<String, Object> params = new HashMap<>();
+        params.put("payment_method", "pm_card_visa");
+        paymentIntent.confirm(params);
+        return paymentIntent;
+    }
+
+    public PaymentIntent cancel(String id) throws StripeException {
+        Stripe.apiKey = secretKey;
+        PaymentIntent paymentIntent = PaymentIntent.retrieve(id);
+        paymentIntent.cancel();
+        return paymentIntent;
+    }
+}
