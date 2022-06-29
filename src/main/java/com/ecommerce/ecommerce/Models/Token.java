3f4f91b56def4dcbc7e797bc7180364beaaf0760
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-29 16:35:13 -0400

    Create Token.java
    Class token that is in PaymentRequest

---
+++ src/main/java/com/ecommerce/ecommerce/Models/Token.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce.Models;
+
+public class Token {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
