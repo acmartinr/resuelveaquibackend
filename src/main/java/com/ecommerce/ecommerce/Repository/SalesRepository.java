ed13030300e4e9c57669a1c0df2cd58661f55ab0
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-30 18:36:39 -0400

    Update SalesRepository.java
    SQL request to fix later

--- src/main/java/com/ecommerce/ecommerce/Repository/SalesRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/SalesRepository.java
@@ -2,6 +2,12 @@ package com.ecommerce.ecommerce.Repository;
 
 import com.ecommerce.ecommerce.Models.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SalesRepository extends JpaRepository<Sale, Long> {
+    /*@Query("select x from Sale x where x.shopping_id like: param")
+    public abstract List<Sale> shopUser(@Param("param")Long id);*/
 }
