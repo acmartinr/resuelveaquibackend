94bf440dbef73dd4e59a5e9296f69eb1c054dee7
Gabriel <gmonteagudomartinez96@gmail.com>
2022-06-30 18:33:46 -0400

    Update SalesController.java
    Were added method getShopUser and all that is relative to Stripe were commented

--- src/main/java/com/ecommerce/ecommerce/Controller/SalesController.java
+++ src/main/java/com/ecommerce/ecommerce/Controller/SalesController.java
@@ -1,9 +1,10 @@
-package com.ecommerce.ecommerce.Security.Controller;
+package com.ecommerce.ecommerce.Controller;
 
 import com.ecommerce.ecommerce.Models.*;
 import com.ecommerce.ecommerce.Repository.*;
 import com.ecommerce.ecommerce.Services.PaymentService;
 import com.ecommerce.ecommerce.Services.ProductoService;
+import com.ecommerce.ecommerce.Utils.PDFGenerator;
 import com.stripe.exception.StripeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.util.*;
 
@@ -42,15 +44,16 @@ public class SalesController {
         return ResponseEntity.ok(sales);
     }
 
+    /*@RequestPart PaymentRequest request*/
     @PostMapping(value="/create_sale")
     public ResponseEntity<String> createSale(@RequestPart ProductSold[] productos,@RequestPart double amount,
-                                             @RequestPart Long id,@RequestPart PaymentRequest request) throws IOException, StripeException {
+                                             @RequestPart Long user_id) throws Exception {
         Sale s=new Sale();
         Sale sale=new Sale();
         Set<ProductSold> products=new HashSet<>(Arrays.asList(productos));
         sale.setAmount(amount);
         //sale.setProductsSolds(products);
-        sale.setUserShopping(userRepository.findById(id).get());
+        sale.setUserShopping(userRepository.findById(user_id).get());
         s=salesRepository.save(sale);
         for (ProductSold productSold:productos){
             Producto pr=productoRepository.findById(productSold.getId()).get();
@@ -62,10 +65,16 @@ public class SalesController {
             productSold.setProduct(pr);
             productSoldRepository.save(productSold);
         }
-        String chargeId= service.charge(request);
-        return chargeId!=null? new ResponseEntity<String>(chargeId, HttpStatus.OK):
-                new ResponseEntity<String>("Please check the credit card details entered",HttpStatus.BAD_REQUEST);
-        //return ResponseEntity.ok("Venta realizada correctamente");
+        //String chargeId= service.charge(request);
+       // if(chargeId!=null){
+         PDFGenerator.generatePDF(s, productos, amount);
+        // return new ResponseEntity<String>(chargeId, HttpStatus.OK);
+        //}
+         //else
+         //return new ResponseEntity<String>("Please check the credit card details entered",HttpStatus.BAD_REQUEST);
+       /* return chargeId!=null? new ResponseEntity<String>(chargeId, HttpStatus.OK):
+                new ResponseEntity<String>("Please check the credit card details entered",HttpStatus.BAD_REQUEST);*/
+        return ResponseEntity.ok("Venta realizada correctamente");
     }
 
     /*@PostMapping(value="/payment")
@@ -96,9 +105,21 @@ public class SalesController {
         return ResponseEntity.ok("Producto agregado al carrito correctamente" );
     }
 
+    @GetMapping(value = "/getShopUser")
+    public List<Sale> getShopUser(@RequestPart Long id) {
+        List<Sale> ls=new ArrayList<>();
+        //ls=salesRepository.shopUser(id);
+        List<Sale> sales = salesRepository.findAll();
+        for(Sale sale:sales){
+            if (sale.getUserShopping().getId()==id)
+                ls.add(sale);
+        }
+        return ls;
+    }
+
     @GetMapping(value = "/limpiar")
-    public ResponseEntity<ShoppingCar> limpiarCarrito(@RequestPart Long id) {
+    public void limpiarCarrito(@RequestPart Long id) {
         ShoppingCar shoppingCar = new ShoppingCar();
-        return ResponseEntity.ok(shoppingCar);
+        userRepository.findById(id).get().setShoppingCar(shoppingCar);
     }
 }
