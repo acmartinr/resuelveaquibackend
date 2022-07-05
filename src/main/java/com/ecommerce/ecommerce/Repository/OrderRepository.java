74b82d50040c9525af187f27fa68457c3259f4a3
Gabriel <gmonteagudomartinez96@gmail.com>
2022-07-05 12:20:37 -0400

    Update OrderRepository.java
    A new method with  a query

--- src/main/java/com/ecommerce/ecommerce/Repository/OrderRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.ecommerce.ecommerce.Repository;
 
 import com.ecommerce.ecommerce.Models.Order;
+import com.ecommerce.ecommerce.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Query("select o from Order o join User u on o.userOrder=:param")
+     List<Order> orderByUser(@Param("param")User user);
+
 }
