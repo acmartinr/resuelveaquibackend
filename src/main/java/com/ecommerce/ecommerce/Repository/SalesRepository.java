a0dcd0944ad5a8ed6199bb24d7dee448da4380eb
Gabriel <gmonteagudomartinez96@gmail.com>
2022-07-05 16:27:27 -0400

    Update SalesRepository.java
    The query has been modified to find all shoppings that belongs to an user

--- src/main/java/com/ecommerce/ecommerce/Repository/SalesRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/SalesRepository.java
@@ -1,6 +1,7 @@
 package com.ecommerce.ecommerce.Repository;
 
 import com.ecommerce.ecommerce.Models.Sale;
+import com.ecommerce.ecommerce.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SalesRepository extends JpaRepository<Sale, Long> {
-    /*@Query("select x from Sale x where x.shopping_id like: param")
-    public abstract List<Sale> shopUser(@Param("param")Long id);*/
+    @Query("select s from Sale s join User u on s.shopping=:param")
+     List<Sale> saleByUser(@Param("param") User user);
 }
