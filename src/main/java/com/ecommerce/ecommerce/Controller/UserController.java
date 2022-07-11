26854c6b6885bb899be60a1f2775790c2ec50783
Gabriel <gmonteagudomartinez96@gmail.com>
2022-07-11 11:39:17 -0400

    Update UserController.java
    changePassword method was added

--- src/main/java/com/ecommerce/ecommerce/Controller/UserController.java
+++ src/main/java/com/ecommerce/ecommerce/Controller/UserController.java
@@ -1,13 +1,18 @@
 package com.ecommerce.ecommerce.Controller;
 
+import com.ecommerce.ecommerce.Models.ChangePasswordDTO;
+import com.ecommerce.ecommerce.Models.Mensaje;
 import com.ecommerce.ecommerce.Models.User;
 import com.ecommerce.ecommerce.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -21,6 +26,8 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @CrossOrigin
     @GetMapping("/all")
@@ -67,5 +74,22 @@ public class UserController {
         else
             userService.delete(id);
         return ResponseEntity.ok(Boolean.TRUE);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordDTO dto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+        if(!dto.getPassword().equals(dto.getConfirmPassword()))
+            return new ResponseEntity(new Mensaje("Las contraseñas no coinciden"), HttpStatus.BAD_REQUEST);
+        Optional<User> usuarioOpt = userService.getByTokenPassword(dto.getTokenPassword());
+        if(!usuarioOpt.isPresent())
+            return new ResponseEntity(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        User user = usuarioOpt.get();
+        String newPassword = passwordEncoder.encode(dto.getPassword());
+        user.setPassword(newPassword);
+        user.setToken(null);
+        userService.save(user);
+        return new ResponseEntity(new Mensaje("Contraseña actualizada"), HttpStatus.OK);
     }
 }
