fd2694a4a6a584133593e4abc951d533b767787a
Gabriel <gmonteagudomartinez96@gmail.com>
2022-07-11 15:18:55 -0400

    Update ProductoRepository.java
    this needs more work

--- src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
@@ -1,8 +1,16 @@
 package com.ecommerce.ecommerce.Repository;
 
 import com.ecommerce.ecommerce.Models.Producto;
+import com.ecommerce.ecommerce.Models.Sale;
+import com.ecommerce.ecommerce.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
     Producto findFirstByCode(String codigo);
+    @Query("select distinct p from Producto p where UPPER(p.name) =UPPER(:param)")
+    List<Producto> findByProductName(@Param("param") String name);
 }
