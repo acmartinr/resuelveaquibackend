9eca1bf9d23a83219187bfb805bfce6ffd216d5c
Gabriel <gmonteagudomartinez96@gmail.com>
2022-07-12 14:43:06 -0400

    Update ProductoRepository.java
    abstract method findByProductName

--- src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
+++ src/main/java/com/ecommerce/ecommerce/Repository/ProductoRepository.java
@@ -3,6 +3,8 @@ package com.ecommerce.ecommerce.Repository;
 import com.ecommerce.ecommerce.Models.Producto;
 import com.ecommerce.ecommerce.Models.Sale;
 import com.ecommerce.ecommerce.Models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +13,6 @@ import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Long> {
     Producto findFirstByCode(String codigo);
-    @Query("select distinct p from Producto p where UPPER(p.name) =UPPER(:param)")
-    List<Producto> findByProductName(@Param("param") String name);
+    @Query("select distinct p from Producto p where Upper(p.name) like %:param%")
+    Page<Producto> findByProductName(@Param("param") String name, Pageable pageable);
 }
