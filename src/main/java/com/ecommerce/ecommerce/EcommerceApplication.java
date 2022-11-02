d4fe997f55a400a798dca169a7f678cb2ec2ff59
gabriel73-art <104366878+gabriel73-art@users.noreply.github.com>
2022-11-02 09:20:29 -0400

    Update EcommerceApplication.java
    Method requestContextListener has been added and extends of SpringBootServletInitializer

--- src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/ecommerce/EcommerceApplication.java
@@ -2,12 +2,22 @@ package com.ecommerce.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
+	}
+
 }
+
+
