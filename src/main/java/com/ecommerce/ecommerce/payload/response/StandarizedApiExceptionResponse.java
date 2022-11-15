d6279f926bf2b2a424d794f0afeac0dc8e5db369
acmartinr <albertocarlosmartin@gmail.com>
2022-11-15 09:18:52 +0100

    added api doc implementation and fixed security level for users and products

---
+++ src/main/java/com/ecommerce/ecommerce/payload/response/StandarizedApiExceptionResponse.java
@@ -0,0 +1,58 @@
+package com.ecommerce.ecommerce.payload.response;
+
+
+public class StandarizedApiExceptionResponse {
+    public String type = "/errors/uncategorized";
+    public String title;
+    public String code;
+    public String detail;
+    public String instance = "/errors/uncategorized/resuelveaqui";
+
+    public StandarizedApiExceptionResponse(String title, String code, String detail) {
+        super();
+        this.title = title;
+        this.code = code;
+        this.detail = detail;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getInstance() {
+        return instance;
+    }
+
+    public void setInstance(String instance) {
+        this.instance = instance;
+    }
+
+}
