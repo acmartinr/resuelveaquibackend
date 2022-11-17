4be8ed8984c133fd7bb442a8a26c0fe9e5ee0aa0
acmartinr <albertocarlosmartin@gmail.com>
2022-11-17 11:18:07 +0100

    added handler for blank or missing fields

--- src/main/java/com/ecommerce/ecommerce/handlers/ApiExceptionHandler.java
+++ src/main/java/com/ecommerce/ecommerce/handlers/ApiExceptionHandler.java
@@ -3,25 +3,29 @@ package com.ecommerce.ecommerce.handlers;
 import com.ecommerce.ecommerce.payload.response.StandarizedApiExceptionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    //Validator for params field
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandarizedApiExceptionResponse> handleMissingParams(MethodArgumentNotValidException  ex) {
+        StandarizedApiExceptionResponse response = new StandarizedApiExceptionResponse("Campos necesarios del rquest vacios o inexistentes", "1025",ex.getMessage());
+        return new ResponseEntity(response, HttpStatus.PARTIAL_CONTENT);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<StandarizedApiExceptionResponse> handleNoContentExcpeption(Exception ex) {
         StandarizedApiExceptionResponse response = new StandarizedApiExceptionResponse("Validation error", "1024",ex.getMessage());
-        return new ResponseEntity(response, HttpStatus.PARTIAL_CONTENT);
-    }
-/*
-    //Validator for params field
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<StandarizedApiExceptionResponse> handleMissingParams(MissingServletRequestParameterException ex) {
-        StandarizedApiExceptionResponse response = new StandarizedApiExceptionResponse("Missing input fields", "1025",ex.getMessage());
         return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
     }
+/*
+
     //Validator for number fields
     @ExceptionHandler(value = NumberFormatException.class)
     public ResponseEntity<StandarizedApiExceptionResponse> httpMessageNotReadable(NumberFormatException ex) {
