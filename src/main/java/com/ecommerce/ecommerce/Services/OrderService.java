c30db6969fd906ccef1990289ba4eac3d58ecbeb
acmartinr <albertocarlosmartin@gmail.com>
2022-11-17 15:26:24 +0100

    added business logic for generate order and pdf

--- src/main/java/com/ecommerce/ecommerce/Services/OrderService.java
+++ src/main/java/com/ecommerce/ecommerce/Services/OrderService.java
@@ -5,11 +5,14 @@ import com.ecommerce.ecommerce.Models.ProductSold;
 import com.ecommerce.ecommerce.Models.Sale;
 import com.ecommerce.ecommerce.Models.User;
 import com.ecommerce.ecommerce.Repository.OrderRepository;
+import com.ecommerce.ecommerce.Security.services.PdfGenerateService;
 import com.ecommerce.ecommerce.Utils.PDFGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -17,6 +20,8 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private PdfGenerateService pdfGenerateService;
 
     public Order create(Order order) {
         return orderRepository.save(order);
@@ -39,11 +44,18 @@ public class OrderService {
 
     public void generatePdf(Order order, Sale saleObject, List<ProductSold> productsAfterSold, double amount, User user) {
         try {
-            String pdf = PDFGenerator.generatePDF(saleObject, productsAfterSold, amount);
+            Map<String, Object> data = new HashMap<>();
+            data.put("productSold", productsAfterSold);
+            data.put("order", order);
+            data.put("user", user);
+            data.put("amount", amount);
+
+            String pdf=java.util.UUID.randomUUID().toString();
             order.setUserOrder(user);
             order.setSale(saleObject);
             order.setDateAndTime(saleObject.getDateAndTime());
-            order.setBill_payment(pdf);
+            order.setBill_payment(pdf+".pdf");
+            pdfGenerateService.generatePdfFile("pdf", data, pdf+".pdf");
             orderRepository.save(order);
         } catch (Exception e) {
             e.printStackTrace();
