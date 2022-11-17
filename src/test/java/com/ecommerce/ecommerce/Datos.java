308c205b2d7a8dc2b3d6dbd63d341ae8b13aeee3
acmartinr <albertocarlosmartin@gmail.com>
2022-11-17 08:28:56 +0100

    added test class

---
+++ src/test/java/com/ecommerce/ecommerce/Datos.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce;
+
+import com.ecommerce.ecommerce.Models.ProductSold;
+import com.ecommerce.ecommerce.Models.Producto;
+import com.ecommerce.ecommerce.Models.Sale;
+import com.ecommerce.ecommerce.Models.User;
+
+public class Datos {
+    public static final User testUser = new User(1L,"Alberto","Martin","Reparto num 11","acmartin", "albertocarlosmartin@gmail.com", "12345678","ROLE_ADMIN");
+    public static final Producto productTest1 = new Producto(1L,"laptop",100.00,100,"1234","electronicos",true);
+    public static final Producto productTest2 = new Producto(2L,"smartphone",200.00,50,"1034","electronicos",false);
+    public static final Producto productTest3 = new Producto(3L,"camisa",100.00,2,"1244","ropa",true);
+    public static final Producto productTest4 = new Producto(4L,"silla",100.00,900,"1234","hogar",false);
+
+    public static final ProductSold productSoldTest1 = new ProductSold(1L,10);
+    public static final ProductSold productSoldTest2 = new ProductSold(2L,30);
+    public static final ProductSold productSoldTest3 = new ProductSold(3L,12);
+    public static final ProductSold productSoldTest4 = new ProductSold(4L,8);
+
+    public static final Sale saleObjectTest = new Sale();
+}
---
+++ src/test/java/com/ecommerce/ecommerce/Services/ProductSoldServiceUTest.java
@@ -0,0 +1,77 @@
+package com.ecommerce.ecommerce.Services;
+
+
+import com.ecommerce.ecommerce.Datos;
+import com.ecommerce.ecommerce.Models.ProductSold;
+import com.ecommerce.ecommerce.Models.Producto;
+import com.ecommerce.ecommerce.Models.Sale;
+import com.ecommerce.ecommerce.Models.User;
+import com.ecommerce.ecommerce.Repository.ProductSoldRepository;
+import com.ecommerce.ecommerce.Repository.ProductoRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+
+@SpringBootTest
+public class ProductSoldServiceUTest {
+    @Autowired
+    private ProductSoldService productSoldService;
+    @MockBean
+    private ProductoRepository productoRepository;
+    @MockBean
+    private ProductSoldRepository productoSoldRepository;
+
+    @Test
+    @WithMockUser(username="admin",roles={"USER","ADMIN"})
+    void should_add_all_products_to_products_sold_error_less_products_than_sold() throws Exception {
+        ProductSold productos[] = new ProductSold[4];
+        productos[0] = Datos.productSoldTest1;
+        productos[1] = Datos.productSoldTest2;
+        productos[2] = Datos.productSoldTest3;
+        productos[3] = Datos.productSoldTest4;
+        User user = Datos.testUser;
+        Sale sale = new Sale();
+        given(productoRepository.findById(1L)).willReturn(java.util.Optional.of(Datos.productTest1));
+        given(productoRepository.findById(2L)).willReturn(java.util.Optional.of(Datos.productTest2));
+        given(productoRepository.findById(3L)).willReturn(java.util.Optional.of(Datos.productTest3));
+        given(productoRepository.findById(4L)).willReturn(java.util.Optional.of(Datos.productTest4));
+        given(productoSoldRepository.save(ArgumentMatchers.any())).willReturn(Datos.productSoldTest1);
+        List<ProductSold> productsAfterSold = productSoldService.addAllProductSold(productos, user, sale);
+        System.out.println(productsAfterSold.size());
+        assertThat(productsAfterSold.get(0).getQuantity(), is(90));
+    }
+    @Test
+    @WithMockUser(username="admin",roles={"USER","ADMIN"})
+    void should_add_all_products_to_products_sold() throws Exception {
+        ProductSold productos[] = new ProductSold[3];
+        productos[0] = Datos.productSoldTest1;
+        productos[1] = Datos.productSoldTest2;
+        productos[2] = Datos.productSoldTest4;
+        User user = Datos.testUser;
+        Sale sale = new Sale();
+        given(productoRepository.findById(1L)).willReturn(java.util.Optional.of(Datos.productTest1));
+        given(productoRepository.findById(2L)).willReturn(java.util.Optional.of(Datos.productTest2));
+        given(productoRepository.findById(4L)).willReturn(java.util.Optional.of(Datos.productTest4));
+        given(productoSoldRepository.save(ArgumentMatchers.any())).willReturn(Datos.productSoldTest1);
+        List<ProductSold> productsAfterSold = productSoldService.addAllProductSold(productos, user, sale);
+        System.out.println(productsAfterSold.size());
+        assertThat(productsAfterSold.get(0).getProduct().getStock(), is(90));
+        assertThat(productsAfterSold.get(1).getProduct().getStock(), is(20));
+        assertThat(productsAfterSold.get(2).getProduct().getStock(), is(892));
+    }
+}
